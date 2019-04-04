284152cf1ca9392af3ee0cec9e68a5d50d54952f
mileslucey <43838442+mileslucey@users.noreply.github.com>
2019-04-03 23:25:25 -0700

    add comments

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,11 +1,19 @@
 // Store API link
 var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
-function markerSize(mag) {
+// Perform a GET request to the query URL
+d3.json(link, function(data) {
+  // Once we get a response, send the data.features object to the createFeatures function
+  createFeatures(data.features);
+});
+
+// Use a function to establish how large each circle will be
+function circleSize(mag) {
   return mag * 20000;
 }
 
-function markerColor(mag) {
+// Use if statement to choose what color each circle is
+function circleColor(mag) {
   if (mag <= 1) {
       return "#39FF33";
   } else if (mag <= 2) {
@@ -21,25 +29,19 @@ function markerColor(mag) {
   };
 }
 
-// Perform a GET request to the query URL
-d3.json(link, function(data) {
-  // Once we get a response, send the data.features object to the createFeatures function
-  createFeatures(data.features);
-});
 
 function createFeatures(earthquakeData) {
 
   var earthquakes = L.geoJSON(earthquakeData, {
   // Define a function we want to run once for each feature in the features array
   // Give each feature a popup describing the place and time of the earthquake
- onEachFeature : function (feature, layer) {
-
+  onEachFeature : function (feature, layer) {
     layer.bindPopup("<h3>" + feature.properties.place +
-      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" + "<p> Magnitude: " +  feature.properties.mag + "</p>")
+      "</h3><hr><p> Time:" + new Date(feature.properties.time) + "<br> Magnitude: " +  feature.properties.mag +"</p>")
     },     pointToLayer: function (feature, latlng) {
       return new L.circle(latlng,
-        {radius: markerSize(feature.properties.mag),
-        fillColor: markerColor(feature.properties.mag),
+        {radius: circleSize(feature.properties.mag),
+        fillColor: circleColor(feature.properties.mag),
         fillOpacity: 0.7,
         stroke: true,
         weight: 1.5,
@@ -102,8 +104,10 @@ function createMap(earthquakes) {
     collapsed: true
   }).addTo(myMap);
 
+  // Set up legend
   var legend = L.control({position: 'bottomleft'});
 
+  // Create content within legend by looping through density intervals and generating labels wih corresponding colored squares
   legend.onAdd = function () {
   
       var div = L.DomUtil.create('div', 'info legend'),
@@ -111,7 +115,7 @@ function createMap(earthquakes) {
   
       for (var i = 0; i < magnitudes.length; i++) {
           div.innerHTML +=
-              '<i style="background:' + markerColor(magnitudes[i] + 1) + '"></i> ' + 
+              '<i style="background:' + circleColor(magnitudes[i] + 1) + '"></i> ' + 
       + magnitudes[i] + (magnitudes[i + 1] ? ' - ' + magnitudes[i + 1] + '<br>' : ' + ');
       }
   
